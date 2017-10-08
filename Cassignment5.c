ed7954d932f613826b131ea3c99f7ca6cc2a41b7
yb6436 <31675690+yb6436@users.noreply.github.com>
2017-10-09 00:04:47 +0900

    C_5st_assignment
    C_5st_assignment
    파스칼삼각형

---
+++ Cassignment5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int qq(int a, int b)
+{
+	if (a == 0 || b == 0 || a == b)
+		return 1;
+	else
+		return qq(a - 1, b) + qq(a - 1, b - 1);
+
+}
+
+int main()
+{
+	int a, i, j;
+	scanf_s("%d", &a);
+
+	for (i = 0; i < a; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			printf("%d", qq(i, j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
